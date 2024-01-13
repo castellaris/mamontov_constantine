31ab12661004f98ee959be8d8aa7fdb69f6c842c
castellaris <156214293+castellaris@users.noreply.github.com>
2024-01-13 16:48:33 +0300

    Update ecripto.js

--- ecripto.js
+++ ecripto.js
@@ -4,17 +4,17 @@ document.getElementById('form')
  .addEventListener('submit', function(event) {
    event.preventDefault();
 
-   btn.value = 'Sending...';
+   btn.value = 'Отправляется...';
 
    const serviceID = 'default_service';
    const templateID = 'template_l16yv5w';
 
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
-      btn.value = 'Send Email';
+      btn.value = 'Отправить сообщение';
       alert('Sent!');
     }, (err) => {
-      btn.value = 'Send Email';
+      btn.value = 'Отправить сообщение';
       alert(JSON.stringify(err));
     });
 });
